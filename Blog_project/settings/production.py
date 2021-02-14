8cf96dbb1a20cdd6b3ad06d43778317acf5c3bfd
SamuelIZABAYO <samizabayo1@gmail.com>
2021-02-15 00:54:34 +0200

    change allowed hosts

--- Blog_project/settings/production.py
+++ Blog_project/settings/production.py
@@ -3,5 +3,5 @@ import environ
 env = environ.Env(DEBUG=(bool, False))
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = env.list('.herokuapp.com', 'peaceful-headland-57859.herokuapp.com')
-DATABASES = {'default': env.db(), }
+ALLOWED_HOSTS = ['peaceful-headland-57859.herokuapp.com']
+# DATABASES = {'default': env.db(), }
