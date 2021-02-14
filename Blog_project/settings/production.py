8234ece320f4cbd64bc8b7001f5b6c588b9d5486
SamuelIZABAYO <samizabayo1@gmail.com>
2021-02-15 00:37:57 +0200

    reconfigure production allowed hosts

--- Blog_project/settings/production.py
+++ Blog_project/settings/production.py
@@ -1,7 +1,7 @@
 import environ
-from Blog_project.settings.base import *
-env = environ.Env(DEBUG=(bool,False))
-DEBUG=env('DEBUG')
-SECRET_KEY=env('SECRET_KEY')
-ALLOWED_HOSTS=env.list('ALLOWED_HOSTS')
-DATABASES={'default':env.db(),}
+
+env = environ.Env(DEBUG=(bool, False))
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+ALLOWED_HOSTS = env.list('.herokuapp.com', 'peaceful-headland-57859.herokuapp.com')
+DATABASES = {'default': env.db(), }
