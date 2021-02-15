aabe7296676d8165cf11a7cd2f680bd96781ed2b
SamuelIZABAYO <samizabayo1@gmail.com>
2021-02-15 10:35:49 +0200

    project reset

--- Blog_project/settings/production.py
+++
@@ -1,7 +0,0 @@
-import environ
-
-env = environ.Env(DEBUG=(bool, True))
-DEBUG = env('DEBUG')
-SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = ['peaceful-headland-57859.herokuapp.com']
-DATABASES = {'default': env.db(), }
