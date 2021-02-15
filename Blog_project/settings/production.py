0000a29a31cb1ae37d5874fa4c7b362713acec97
SamuelIZABAYO <samizabayo1@gmail.com>
2021-02-15 09:53:15 +0200

    change production debug true

--- Blog_project/settings/production.py
+++ Blog_project/settings/production.py
@@ -1,6 +1,6 @@
 import environ
 
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env(DEBUG=(bool, True))
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 ALLOWED_HOSTS = ['peaceful-headland-57859.herokuapp.com']
